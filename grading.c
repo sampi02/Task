738b0866dd676408e46dcbebce270f5c39f1540d
sampi2005031 <67134016+sampi2005031@users.noreply.github.com>
2020-06-19 08:49:00 +0530

    Regards

---
+++ grading.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter the number of modules\n");
+    scanf("%d",&n);
+    int marks[100];
+    for(i=0;i<n;i++)
+    {
+    printf("Enter your marks ");
+    scanf("%d",&marks[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    if(marks[i]<0 || marks[i]>100)
+    {
+        printf("Wrong Entry\n");
+    }
+    else if(marks[i]<50)
+    {
+        printf("Grade F\n");
+    }
+    else if(marks[i]>=50 && marks[i]<60)
+    {
+        printf("Grade D\n");
+    }
+    else if(marks[i]>=60 && marks[i]<70)
+    {
+        printf("Grade C\n");
+    }
+    else if(marks[i]>=70 && marks[i]<80)
+    {
+        printf("Grade B\n");
+    }
+    else if(marks[i]>=80 && marks[i]<90)
+    {
+        printf("Grade A\n");
+    }
+    else
+    {
+        printf("Grade A+\n");
+    }
+    }
+}
+
+
+
+
