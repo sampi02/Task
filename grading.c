f80bb82e5b4a820848dccad73ceac54889f2fb85
sampi2005031 <67134016+sampi2005031@users.noreply.github.com>
2020-06-19 08:53:46 +0530

    allowing entry for n students

--- grading.c
+++ grading.c
@@ -2,9 +2,14 @@
 void main()
 {
     int n,i;
+    int m;
+    printf("enter the number of students\n");
+    scanf("%d",&m);
     printf("enter the number of modules\n");
     scanf("%d",&n);
     int marks[100];
+    for(int j=0;j<m;j++)
+    {
     for(i=0;i<n;i++)
     {
     printf("Enter your marks ");
